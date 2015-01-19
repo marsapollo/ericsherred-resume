3a46c73f94984f29b3c7df173fdb5d9546f2dcf2
Eric Sherred <esherred@gmail.com>
2015-01-19 23:00:11 +0100

    first

---
+++ index.php
@@ -0,0 +1,322 @@
+<?php $email = 'ericsherred@gmail.com'; ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>EricSherred.com</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+    
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-7400804-2', 'auto');
+      ga('send', 'pageview');
+    </script>
+
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Eric<strong>Sherred</strong></h1>
+        <h2>Digital Strategist and PHP Web Developer</h2>
+        <p><img src="http://www.gravatar.com/avatar/<?php echo md5($email); ?>?s=250" class="img-responsive img-circle" alt="Eric Sherred"></p>
+        <p><strong>New Website Coming Soon</strong></p>
+        <p>
+          <a class="btn btn-primary" target="_blank" href="https://www.facebook.com/sherred"><i class="fa fa-2 fa-facebook"></i></a>
+          <a class="btn btn-primary" target="_blank" href="https://twitter.com/ericsherred"><i class="fa fa-2 fa-twitter"></i></a>
+          <a class="btn btn-primary" target="_blank" href="https://www.linkedin.com/in/esherred"><i class="fa fa-2 fa-linkedin"></i></a>
+          <a class="btn btn-primary" target="_blank" href="https://github.com/esherred"><i class="fa fa-2 fa-github"></i></a>
+          <a class="btn btn-success" href="mailto:ericsherred@gmail.com?subject=EricSherred.com Contact"><i class="fa fa-2 fa-envelope"></i></a>
+          <a class="btn btn-success" href="tel:+12027135756"><i class="fa fa-2 fa-phone"></i></a>
+        </p>
+        <p><strong>Join my mailing list:</strong></p>
+        <div>
+          <form action="//ericsherred.us3.list-manage.com/subscribe/post?u=41d68b6fc55a7f30fbdd27bbd&amp;id=3449807c68" method="POST" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="form-inline validate" role="form" target="_blank" novalidate>
+          
+            <div class="form-group mc-field-group">
+              <label class="sr-only" for="mce-EMAIL">Email</label>
+              <input type="email" class="form-control" name="EMAIL" id="mce-EMAIL" placeholder="Email *">
+            </div>
+
+            <div class="form-group mc-field-group">
+              <label class="sr-only" for="mce-FNAME">First Name</label>
+              <input type="text" class="form-control" name="FNAME" id="mce-FNAME" placeholder="First Name">
+            </div>
+
+            <div class="form-group mc-field-group">
+              <label class="sr-only" for="mce-LNAME">Last Name</label>
+              <input type="text" class="form-control" name="LNAME" id="mce-LNAME" placeholder="Last Name">
+            </div>
+            
+            <div class="form-group">
+              <button type="submit" name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary">Subscribe</button>
+            </div>
+
+            <div id="mce-responses" class="clear">
+              <div class="response" id="mce-error-response" style="display:none"></div>
+              <div class="response" id="mce-success-response" style="display:none"></div>
+            </div>
+
+          </form>
+        </div>
+      </div>
+    </div> <!-- End div.jumbotron -->
+
+    <div class="container">
+
+      <!-- Go to www.addthis.com/dashboard to customize your tools -->
+      <div class="addthis_responsive_sharing"></div>
+
+      <section class="summery">
+        <h2>Digital Résumé</h2>
+        <div class="col-xs-12">
+          <p>Experienced in <strong>Web Development</strong>,<strong> Digital Marketing</strong>, and <strong>Social Media Strategy</strong>. Knowledgeable in <strong>HTML</strong>, <strong>CMS</strong> and <strong>Custom Web Development</strong>. A self-starter with proven problem solving skills. Exceptional instincts but believes in statistical analysis and constant testing. Driven for the best results in every situation. <strong>Core competencies include:</strong></p>
+          <p><strong>PHP Web Development</strong> | <strong>WordPress Development</strong> | <strong>Drupal Development</strong> | <strong>HTML/CSS/JavaScript Development</strong> | <strong>Digital Marketing</strong> | <strong>Google Analytics</strong> | <strong>Digital Strategy</strong></p>
+          <p>
+            <strong>Wprk Examples:</strong><br>
+            <a href="http://www.washingtonian.com/tickets/bow/2014/" target="_blank">http://www.washingtonian.com/tickets/bow/2014/</a> | <a href="http://www.stopthehit.com/" target="_blank">http://www.stopthehit.com/</a>  | <a href="http://www.washingtonian.com/tickets/fitfest/" target="_blank">http://www.washingtonian.com/tickets/fitfest/</a> | <a href="https://bitbucket.org/ericsherred/subforms" target="_blank">https://bitbucket.org/ericsherred/subforms</a>
+          </p>
+        </div>
+      </section> <!--  End section.summery -->
+
+      <section class="skills">
+        <h2>Technical Skills</h2>
+        <div class="col-sm-6">
+          <div class="web">
+            <strong>Web Technologies:</strong>
+            <br>HTML/XHTML/HTML 5/CSS/CSS 3/LESS
+            <br>JavaScript/jQuery
+            <br>Drupal/WordPress/NationBuilder/Blackbaud
+            <br>PHP/C/C++/JAVA
+            <br>Apache/MySQL Sever
+            <br>Website Infrastructure Scaling
+            <br>MAMP/XAMP Stack
+          </div>
+          <div class="certs">
+            <strong>Certifications/Qualifications:</strong>
+            <br>Google AdWords Individual Qualification – Search Advertising
+            <br>Google AdWords Individual Qualification – Display Advertising
+            <br>Google Analytics Individual Qualification
+            <br>CrossFit Level 1 Trainer
+            <br>PADI Divemaster
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="marketing">
+            <strong>Marketing/Advertising:</strong>
+            <br>Google Search Advertising/Google Display Advertising/Google Video Advertising 
+            <br>Yahoo!/Bing Advertising
+            <br>Email Marketing
+            <br>Mobile Marketing/Text Message Marketing
+            <br>Social Media Advertising (Facebook, Twitter, LinkedIn)
+            <br>Social Media Marketing (Facebook, Twitter, LinkedIn, Google+)
+            <br>Search Engine Marketing (SEO)/Search Engine Marketing (SEM)
+            <br>Targeting and Analytics
+            <br>Strategic and Campaign Development
+          </div> 
+          <div class="other">
+            <strong>Other:</strong>
+            <br>Microsoft Office Suite
+            <br>Adobe Creative Cloud
+          </div>
+        </div>
+      </section> <!-- End section.skills -->
+
+      <section class="experience">
+        <h2>Professional Experience</h2>
+        <div class="col-xs-12">
+          
+        <article>
+          <div class="employment">Alley Interactive LLC | New York, NY | 2014 – 2014</div>
+          <div class="title">Software Developer</div>
+          <div class="desc">
+            <ul>
+              <li>Collaborated with a distributed team daily to develop highly customized Drupal websites in the publishing vertical.</li>
+              <li>Wrote custom Drupal Modules for over 8 websites.</li>
+              <li>Specialized in SOLR search integration into the Drupal infrastructure.</li>
+            </ul>
+          </div>
+        </article>
+
+        <article>
+          <div class="employment">Washingtonian Media | Washington, DC | 2013 – 2014</div>
+          <div class="title">Web Developer</div>
+          <div class="desc">
+            <ul>
+              <li>Manage custom PHP and MoveableType development for Washingtonian.com.</li>
+              <li>Develop websites for Washingtonian Custom Media, which runs external projects at the company. Major project is with the Drug Enforcement Agency to maintain two .NET websites while developing new sites on a Drupal CMS system.</li>
+              <li>Build marketing strategies for testing to increase online subscriptions to the magazine by 150% over 3 months.</li>
+            </ul>
+          </div>
+        </article>
+
+        <article>
+          <div class="employment">Signal One, LLC | Woodbridge, VA | 2012 – 2013</div>
+          <div class="title">Founder, Marketer and Developer</div>
+          <div class="desc">
+            <ul>
+              <li>Managed digital communications and online branding for small businesses in the Northern Virginia area.</li>
+              <li>Developed digital strategy for a successful statewide political campaign to improve online presence and maximize client exposure. </li>
+              <li>Worked with campaign and business teams to build online communities around targeted users.</li>
+              <li>Developed websites on the Drupal and WordPress CMS platforms for statewide political candidates and national organizations.</li>
+            </ul>
+          </div>
+        </article>
+
+        <article>
+          <div class="employment">emotive, LLC | Arlington, VA | 2009 – 2012</div>
+          <div class="title">Associate Technology Director/Senior Online Strategist, National Campaigns </div>
+          <div class="desc">
+            <ul>
+              <li>Wrote microtargeted advertising plans for a national audience to increase online donations by an average of 125% and a maximum of 3,000%. </li>
+              <li>Designed text and display ads based on approved advertising plans that targeted local, statewide, and national audiences. </li>
+              <li>Designed, wrote, and developed fundraising emails for political campaigns and national organizations that resulted in an average donation of $50.</li>
+              <li>Developed website designs from Adobe Phodoshop documents to deploy as WordPress, Joomla!, Drupal, Blackbaud Sphere, and Static HTML websites.</li>
+              <li>Converted Adobe Photoshop designs to fully functional websites on WordPress, Joomla!, Drupal and Blackbaud Sphere for statewide campaigns and national organizations.</li>
+              <li>Developed social media outreach plans for social networks such as Facebook and Twitter through post engagement, advertising and website landing pages.</li>
+              <li>Used reporting tools such as Google AdWords, Google Analytics and Facebook Insights to optimize ads and targeting in order to increase the Return on Investment (ROI) of the campaigns by approximately 15%.</li>
+            </ul>
+          </div>
+        </article>
+
+        <article>
+          <div class="employment">National Republican Senatorial Committee | Washington, DC | 2009 – 2009</div>
+          <div class="title">Deputy Director of New Media</div>
+          <div class="desc">
+            <ul>
+              <li>Oversaw day-to-day maintenance of NRSC.org. </li>
+              <li>Redesigned, developed and deployed a new version of NRSC.org through cooperation with vendors and coworkers.</li>
+              <li>Conducted blogger outreach in targeted states to help spread the daily message of the NRSC.</li>
+              <li>Worked directly with the Communications Director and New Media Director to develop email campaigns for fundraising and list growth.</li>
+              <li>Designed, wrote, and developed copy for fundraising and communication email campaigns.</li>
+              <li>Wrote, edited, and formatted copy for email marketing and email communications campaigns. </li>
+              <li>Used Google Analytics and email reporting software to analyze and report on website and email performance. </li>
+              <li>Organized social media outreach on Facebook and Twitter. </li>
+            </ul>
+          </div>
+        </article>
+
+        <article>
+          <div class="employment">Republican National Committee | Washington, DC | 2008 – 2009</div>
+          <div class="title">Web Developer</div>
+          <div class="desc">
+            <ul>
+              <li>Designed, developed and deployed Static HTML websites to highlight research documentation based on communications strategy. </li>
+              <li>Developed Facebook Applications to pair with established microsites based on target audience.</li>
+              <li>Used Google Analytics to track the performance of GOP.com and 10 major micro-sites.</li>
+              <li>Used Facebook and Twitter as a means of communication with users off of GOP.com.</li>
+              <li>Worked directly with the Deputy eCampaign Director on voter engagement strategy and GOP.com redesign strategies.</li>
+            </ul>
+          </div>
+        </article>
+
+        </div>  
+      </section> <!-- End section.experience -->
+
+      <section class="education">
+        <h2>Education</h2>
+        <div class="col-xs-12">
+          <article>
+            <div class="school">Purdue University | West Lafayette, IN | 2012</div>
+            <div class="degree">BA Political Science</div>
+            <div class="desc">Studied Aeronautical/Astronautical Engineering for 4 years during my time at Purdue</div>
+          </article>
+        </div>
+      </section> <!-- End section.education -->
+
+      <section class="activities">
+        <h2>Activities</h2>
+        <div class="col-xs-12">
+
+          <article>
+            <div class="title">CrossFit Level 1 Trainer</div>
+            <div class="desc">Evaluate and coach individuals and classes on the CrossFit method of fitness, functional movement, and nutrition.</div>
+          </article>
+
+          <article>
+            <div class="title">Professional Association of Diving Instructors (PADI) Divemaster</div>
+            <div class="desc">Coordinate with Professional Association of Diving Instructors (PADI) Instructors during confined and open water classes to ensure the safety of the students smooth operation of the class.</div>
+          </article>
+
+          <article>
+            <div class="title">Purdue University Bands</div>
+            <div class="desc">Supply Office Associate Head, Purdue “All-American” Marching Band, Purdue Gold And Black Sound (Fall 2002 – Fall 2007)</div>
+          </article>
+
+          <article>
+            <div class="title">Kappa Kappa Psi, National Honorary Band Fraternity</div>
+            <div class="desc">Offices held: Finances Head, Secretary, Membership Committee Head, Historian, Alumni Coordinator</div>
+          </article>
+
+        </div>
+      </section> <!-- End section.activities -->
+
+    </div>
+
+    <nav class="navbar navbar-default navbar-bottom navbar-fixed-bottom" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Eric<strong>Sherred</strong></a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+              <a class="btn" target="_blank" href="https://www.facebook.com/sherred"><i class="fa fa-facebook"></i><span class="hidden-sm hidden-md hidden-lg"> Facebook</span></a>
+            </li>
+            <li>
+              <a class="btn" target="_blank" href="https://twitter.com/ericsherred"><i class="fa fa-twitter"></i><span class="hidden-sm hidden-md hidden-lg"> Twitter</span></a>
+            </li>
+            <li>
+              <a class="btn" target="_blank" href="https://www.linkedin.com/in/esherred"><i class="fa fa-linkedin"></i><span class="hidden-sm hidden-md hidden-lg"> LinkedIn</span></a>
+            </li>
+            <li>
+              <a class="btn" target="_blank" href="https://github.com/esherred"><i class="fa fa-github"></i><span class="hidden-sm hidden-md hidden-lg"> GitHub</span></a>
+            </li>
+            <li>
+              <a class="btn" href="mailto:ericsherred@gmail.com?subject=EricSherred.com Contact"><i class="fa fa-envelope"></i><span class="hidden-sm hidden-md hidden-lg"> Email</span></a>
+            </li>
+            <li>
+              <a class="btn" href="tel:+12027135756"><i class="fa fa-phone"></i><span class="hidden-sm hidden-md hidden-lg"> Call</span></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- jQuery -->
+    <script src="//code.jquery.com/jquery.js"></script>
+    <!-- Bootstrap JavaScript -->
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+    <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+    <script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+
+    <!-- Go to www.addthis.com/dashboard to customize your tools -->
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-548557ca789ad5b6" async="async"></script>
+
+  </body>
+</html>
